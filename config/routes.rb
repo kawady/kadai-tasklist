88c9dbc4ac7d0764eb822693b9232d0ff69cad16
shutarokawada <comcomkawady@yahoo.co.jp>
2017-04-20 01:55:35 +0000

    201704201055

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,10 @@
 Rails.application.routes.draw do
-  
-  get 'toppages/index'
-
   root to: 'toppages#index'
-  
-  get 'sessions/new'
 
-  get 'sessions/create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
-  get 'sessions/destroy'
-
-  root to: "tasks#index"
-  #resources :tasks
-  
-  get "login", to: "sessions#new#"
-  post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
-  
-  get "signup", to: "users#new"
+  get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
 end
