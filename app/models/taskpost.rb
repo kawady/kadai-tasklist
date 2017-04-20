d3891b70fe5f5d6f7e9c310234b90f27a82f03de
shutarokawada <comcomkawady@yahoo.co.jp>
2017-04-20 06:50:45 +0000

    before_edit

---
+++ app/models/taskpost.rb
@@ -0,0 +1,8 @@
+class Taskpost < ApplicationRecord
+  belongs_to :user
+  
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 255 }
+  validates :status, presence: true
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420011639) do
+ActiveRecord::Schema.define(version: 20170420063212) do
+
+  create_table "taskposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "status"
+    t.index ["user_id"], name: "index_taskposts_on_user_id", using: :btree
+  end
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 20170420011639) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "taskposts", "users"
 end
